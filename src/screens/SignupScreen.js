721afd7ff9e664c28a20cac1a96fd766538a9b2e
rpattcorner <robert.patt-corner@eglobaltech.com>
2020-11-23 17:25:57 -0500

    initial navigation

---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, StyleSheet, Text, Button } from 'react-native';
+
+const SignupScreen = ({ navigation }) => {
+    return <>
+        <Text style={{ fontSize: 48 }}SignupScreen></Text>
+        <Button 
+            title="Go to Signin" 
+            onPress={() => navigation.navigate('Signin')} 
+        />
+        <Button 
+            title="Go to Main Flow" 
+            onPress={() => navigation.navigate('mainFlow')} 
+        />
+    </>
+};
+
+const styles=StyleSheet.create({});
+
+export default SignupScreen;
