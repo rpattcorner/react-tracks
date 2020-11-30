524707cdb94f94a9e8bbcc03e4cf5deb5e15451c
rpattcorner <robert.patt-corner@eglobaltech.com>
2020-11-30 15:11:08 -0500

    tracks basic signup formatting

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,20 +1,56 @@
-import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements';
+import Spacer from '../components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
-    return <>
-        <Text style={{ fontSize: 48 }}SignupScreen></Text>
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    return <View style={styles.container}>
+    <Spacer><Text h3> Sign Up for Tracker</Text></Spacer>
+    <Input 
+        label='EMail'
+        value={email}
+        onChangeText={(newEmail) => setEmail(newEmail)}
+        autoCapitalize="none"
+        autoCorrect={false}
+     />
+    <Spacer />
+            <Input 
+                label='Password'
+                value={password}
+                onChangeText={setPassword}
+                autoCapitalize="none"
+                autoCorrect={false}
+                secureTextEntry
+            />
+    <Spacer />
+        <Button title="Sign Up" />
+    <Spacer />
+
+        {/* <Text style={{ fontSize: 48 }}SignupScreen></Text>
         <Button 
-            title="Go to Signin" 
+            title="Go to Signin"  
             onPress={() => navigation.navigate('Signin')} 
         />
         <Button 
             title="Go to Main Flow" 
             onPress={() => navigation.navigate('mainFlow')} 
-        />
-    </>
+        /> */}
+    </View>
 };
 
-const styles=StyleSheet.create({});
+SignupScreen.navigationOptions = { 
+    headerShown: false
+};
+
+const styles=StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 100
+    }
+});
 
 export default SignupScreen;
