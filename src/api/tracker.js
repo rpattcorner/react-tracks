401ae10c92bbfc28a3cdbccb8d6210b88afcccea
rpattcorner <robert.patt-corner@eglobaltech.com>
2020-12-11 09:33:58 -0500

    add circle for location

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://62249ba9f10f.ngrok.io',
+  baseURL: 'http://368705b31a75.ngrok.io',
 });
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { Text, StyleSheet, ActivityIndicator } from 'react-native';
-import MapView, { Polyline } from 'react-native-maps';
+import MapView, { Polyline, Circle } from 'react-native-maps';
 import { Context as LocationContext } from '../context/LocationContext';
 
 const Map = () => {
@@ -46,6 +46,13 @@ const Map = () => {
             }}
         >
             {/* <Polyline coordinates={points}/> */}
+            <Circle
+                center={currentLocation.coords}
+                radius={25}
+                strokeColor={"rgba(158, 158, 255, 1.0)"}
+                fillColor={"rgba(158, 158, 255, 0.3)"}
+
+            />
         </MapView>
     );
 };
